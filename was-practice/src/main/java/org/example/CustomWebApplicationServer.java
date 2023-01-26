de660dc3bb61300a12892256ed040151cc688865
wlsgur073 <wlsgur073@gmail.com>
2023-01-26 22:01:25 +0900

    added Thread

--- was-practice/src/main/java/org/example/CustomWebApplicationServer.java
+++ was-practice/src/main/java/org/example/CustomWebApplicationServer.java
@@ -20,43 +20,25 @@ public class CustomWebApplicationServer {
     }
 
     public void start() throws IOException {
-        try(ServerSocket serverSocket = new ServerSocket(port)){
+        try (ServerSocket serverSocket = new ServerSocket(port)) {
             logger.info("[CustomWebApplicationServer] started {} port.", port);
 
             Socket clientSocket;
             logger.info("[CustomWebApplicationServer] waiting for client.");
 
-            while ((clientSocket = serverSocket.accept()) != null){
+            while ((clientSocket = serverSocket.accept()) != null) {
                 logger.info("[CustomWebApplicationServer] client connected!");
 
 //                Step1 - 사용자 요청을 메인 Thread가 처리하도록 한다.
-                try (InputStream in = clientSocket.getInputStream(); OutputStream out = clientSocket.getOutputStream()){
-                    BufferedReader br = new BufferedReader(new InputStreamReader(in, StandardCharsets.UTF_8));
-                    DataOutputStream dos = new DataOutputStream(out);
+//                Step2는  ClientRequestHandler를 만들어서 그쪽 Thread에 넣어주었다.
 
-//                    String line;
-//                    while(!(line = br.readLine()).equals("")){
-//                        System.out.println(line);
-//                    }
+//                아래와 같이 하면, 사용자의 요청이 들어올때마다 Thread가 새로 생성되어
+//                생성될때마다 독립적인 메모리를 할당받고 요청이 많아질수록 성능을 매우 저하시킨다.
+                new Thread(new ClientRequestHandler(clientSocket)).start();
 
-                    HttpRequest httpRequest = new HttpRequest(br);
-
-                    // GET /calculate?operand1=11&operator=*&operand2=55
-                    if (httpRequest.isGetRequest() && httpRequest.matchPath("/calculate")){
-                        QueryStrings queryStrings = httpRequest.getQueryString();
-
-                        int operand1 = Integer.parseInt(queryStrings.getValue("operand1"));
-                        String operator = queryStrings.getValue("operator");
-                        int operand2 = Integer.parseInt(queryStrings.getValue("operand2"));
-
-                        int result = Calculator.calculate(new PositiveNumber(operand1), operator, new PositiveNumber(operand2));
-                        byte[] body = String.valueOf(result).getBytes();
-
-                        HttpResponse response = new HttpResponse(dos);
-                        response.response200Header("application/json", body.length);
-                        response.responseBody(body);
-                    }
-                }
+//                따라서 Thread를 고정된 갯수만큼 생성해두고 이를 재활용하는 Thread Pool 개념을 적용해보자.
+//                Step3 - Thread Pool을 적용해 안정적인 서비스가 가능하도록 한다.
+                
             }
         }
     }
